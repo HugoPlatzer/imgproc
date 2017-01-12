d72446fb7ece3e121e0295ed57937c676f40f78f
Hugo Platzer <hplatzer@cosy.sbg.ac.at>
2017-01-12 13:28:18 +0100

    swt transform supported

--- fv.py
+++ fv.py
@@ -29,6 +29,14 @@ def procChanDWT(img, wavelet, nLevels):
             fv.append(np.mean(np.abs(x - np.mean(x))))
     return fv
     
+def procChanSWT(img, wavelet, nLevels):
+    wt = pywt.swt2(img, wavelet=wavelet, level=nLevels)
+    fv = []
+    for l in xrange(1, nLevels):
+        for x in wt[l][1]:
+            fv.append(np.std(x))
+            fv.append(np.mean(np.abs(x - np.mean(x))))
+    return fv
 
 def procImg(filename, colorSpace, wavelet, nLevels):
     img = cv2.imread(filename, cv2.IMREAD_COLOR)
@@ -39,27 +47,29 @@ def procImg(filename, colorSpace, wavelet, nLevels):
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
         ic = clahe.apply(ic)
         ic = cv2.normalize(ic, dst=ic, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        fv += procChanDWT(ic, wavelet, nLevels)
+        fv += transformTypeMapping[args.transformType](ic, wavelet, nLevels)
     return fv
 
 
-def processPictures(patientMapping, imageDir, patternClass, args):
+def processPictures(patientMapping, imageDir, patternClass):
     for imageFileName in os.listdir(imageDir):
         fv = procImg(os.path.join(imageDir, imageFileName), args.colorSpace, args.waveletFunction, int(args.nLevels))
         print("{} {} {}".format(patternClass, patientMapping[imageFileName], " ".join(str(k) for k in fv)))
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("transformType", choices = ["dwt", "swt"])
-parser.add_argument("colorSpace")
-parser.add_argument("waveletFunction")
-parser.add_argument("nLevels", type = int)
-args = parser.parse_args()
 
 dataDir = "img"
 patientMapping = loadPatientMapping(os.path.join(dataDir, "patientmapping.csv"))
 patternClassMapping = {"Pit Pattern I": "A", "Pit Pattern II": "A",
           "Pit Pattern III L": "B", "Pit Pattern III S": "B",
           "Pit Pattern IV": "B", "Pit Pattern V": "C"}
+transformTypeMapping = {"swt" : procChanSWT, "dwt" : procChanDWT}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("transformType", choices = transformTypeMapping.keys())
+parser.add_argument("colorSpace")
+parser.add_argument("waveletFunction")
+parser.add_argument("nLevels", type = int)
+args = parser.parse_args()
+
 for patternDir, patternClass in patternClassMapping.iteritems():
-    processPictures(patientMapping, os.path.join(dataDir, patternDir), patternClass, args)
+    processPictures(patientMapping, os.path.join(dataDir, patternDir), patternClass)
